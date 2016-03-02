8190be6f7c57f637ad22758b3068f0987e560b49
amauryESGI <amaury.colmant@wanadoo.fr>
2016-03-02 10:01:32 +0100

    Set directories

--- Domineering/Assets/Minamax.cs
+++ Domineering/Assets/Minamax.cs
@@ -1,43 +1,43 @@
-﻿using UnityEditor;
+﻿//using UnityEditor;
 
-public class Minamax {
-    private int _valMin;
-    private int _valMax;
+//public class Minamax {
+//    private int _valMin;
+//    private int _valMax;
 
-    public Minamax(int valMin, int valMax) {
-        _valMin = valMin;
-        _valMax = valMax;
-    }
+//    public Minamax(int valMin, int valMax) {
+//        _valMin = valMin;
+//        _valMax = valMax;
+//    }
 
-    public int Min(int depth) {
-        if (depth == 0)
-            return evalution();
+//    public int Min(int depth) {
+//        if (depth == 0)
+//            return evalution();
 
-        int eval = _valMax;
+//        int eval = _valMax;
 
-        foreach (var move in possibleMoves) {
-            play(m);
-            int e = Max(depth);
-            undo(m);
-            if (e < eval)
-                eval = e;
-        }
-        return eval;
-    }
+//        foreach (var move in possibleMoves) {
+//            play(m);
+//            int e = Max(depth);
+//            undo(m);
+//            if (e < eval)
+//                eval = e;
+//        }
+//        return eval;
+//    }
 
-    public int Max(int depth) {
-        if (depth == 0)
-            return evalution();
+//    public int Max(int depth) {
+//        if (depth == 0)
+//            return evalution();
 
-        int eval = _valMin;
+//        int eval = _valMin;
 
-        foreach (var move in possibleMoves) {
-            play(m);
-            int e = Min(depth);
-            undo(m);
-            if (e > eval)
-                eval = e;
-        }
-        return eval;
-    }
-}
+//        foreach (var move in possibleMoves) {
+//            play(m);
+//            int e = Min(depth);
+//            undo(m);
+//            if (e > eval)
+//                eval = e;
+//        }
+//        return eval;
+//    }
+//}
